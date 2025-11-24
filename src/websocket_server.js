e157b6c67074dab46749f1c8a0b04cef5c9bb591
sarvbesh <atsarveshyadav@gmail.com>
2025-11-24 15:38:58 +0530

    Implement function to build unmasked data frames

--- src/websocket_server.js
+++ src/websocket_server.js
@@ -19,3 +19,27 @@ const server = http.createServer((req, res) => {
   res.writeHead(404);
   res.end("Please upgrade the websocket!");
 });
+
+const buildFrame = ({ opcode, payload = Buffer.alloc(0), fin = true}) => {
+  // first byte
+  const first = (fin ? 0x80 : 0x00) | (opcode & 0x0f);
+  const len = payload.length;
+
+  /* if payload length < 126, only send 2 byte header and data */
+  if(len < 126) {
+    return Buffer.concat([Buffer.from([first, length]), payload]);
+  } else if (len <= 0xffff) {
+    const h = Buffer.alloc(4);
+    h[0] = first;
+    h[1] = 126; // 2nd byte
+    h.writeUInt16BE(len, 2); // actual length at offset 2
+    return Buffer.concat([h, payload]);
+  } else {
+    const h = Buffer.alloc(10);
+    h[0] = first;
+    h[1] = 127; // 2nd byte
+    h.writeUInt32BE(0, 2); // high 4 bytes = 0 (not handling > 4GB)
+    h.writeUInt32BE(len, 6); // low 4 bytes = actual length
+    return Buffer.concat([h, payload]);
+  }
+};
